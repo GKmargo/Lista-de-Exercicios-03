fccc58e0dbee4a0c899280069c3df9df5eaf5776
GKmargo <giovanitccamargo@gmail.com>
2024-03-21 14:06:51 -0300

    Exercicio 07

--- ImpostoCarro.java
+++ ImpostoCarro.java
@@ -1,7 +1,7 @@
 public class ImpostoCarro {
     public double valorFabrica;
-    public double distribuidor = 28.0 / 100;
-    public double imposto = 45.0 / 100;
+    public double distribuidor = 0.28;
+    public double imposto = 0.45;
 
     public double carroNovo(double valorFabrica){
         double valorImposto = valorFabrica * imposto;
