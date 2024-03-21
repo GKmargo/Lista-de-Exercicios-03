bebc8037ffe78aa7d7bfe1d9585e89bc85725450
GKmargo <giovanitccamargo@gmail.com>
2024-03-21 09:51:07 -0300

    Exercicio 02

--- Carro.java
+++ Carro.java
@@ -2,7 +2,7 @@ public class Carro{
     public double km;
     public double litros;
 
-    public static double consumoMedio (double km, double litros){
+    public double consumoMedio (double km, double litros){
         return km/litros;
     } 
 }
--- Exercicio02.java
+++ Exercicio02.java
@@ -5,6 +5,6 @@ public class Exercicio02{
         carro.km = Prompt.lerDecimal("Digite a distancia percorrida pelo seu carro:");
         carro.litros = Prompt.lerDecimal("Digite a quantidade de litros de gasolina gastos com a viagem");
 
-        Prompt.imprimir("O consumo médio do seu veículo: " + Carro.consumoMedio(carro.km, carro.litros) + "km/l");
+        Prompt.imprimir("O consumo médio do seu veículo: " + carro.consumoMedio(carro.km, carro.litros) + "km/l");
     }
 }
