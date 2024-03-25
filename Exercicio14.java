3c1632028dd02cc4e94bc5da0dc8eb6a80e09d5b
GKmargo <giovanitccamargo@gmail.com>
2024-03-25 16:48:49 -0300

    Exercicio 14

---
+++ Exercicio14.java
@@ -0,0 +1,27 @@
+public class Exercicio14 {
+    public static void executar(){
+        Produto[] produtos = new Produto[40];
+        double totalPrecoCusto = 0;
+        double totalPrecoVenda = 0;
+
+        for (int i = 0; i < produtos.length; i++) {
+            double precoCusto = Prompt.lerDecimal("Digite o preço de custo do produto " + (i + 1) + ":");
+            
+            double precoVenda = Prompt.lerDecimal("Digite o preço de venda do produto " + (i + 1) + ":");
+            
+             produtos[i] = new Produto();
+             produtos[i].precoCusto = precoCusto;
+             produtos[i].precoVenda = precoVenda;
+             totalPrecoCusto += precoCusto;
+             totalPrecoVenda += precoVenda;
+
+            Prompt.imprimir("Produto " + (i + 1) + ": " + produtos[i].calculoLucro());
+        }
+
+        double mediaPrecoCusto = totalPrecoCusto / produtos.length;
+        double mediaPrecoVenda = totalPrecoVenda / produtos.length;
+
+        Prompt.imprimir("Média do preço de custo: " + mediaPrecoCusto);
+        Prompt.imprimir("Média do preço de venda: " + mediaPrecoVenda);
+    }
+}
--- Produto.java
+++ Produto.java
@@ -1,9 +1,22 @@
 public class Produto{
     public double valor;
     public double valorPorcentagem;
-
+    
     public double acrescimo(double valor, double valorPorcentagem){
         double porcentagem = (valor * valorPorcentagem) /100;
         return valor + porcentagem;
+    }
+    
+    public double precoCusto;
+    public double precoVenda;
+    
+    public String calculoLucro(){
+        if (precoVenda > precoCusto){
+            return "Lucro";
+        } else if (precoVenda < precoCusto){
+            return "Prejuizo";
+        } else {
+            return "Empate";
+        }
     }
 }
